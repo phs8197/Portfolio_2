7c3e7360a9475acfc52b5733c41e1dbc34396003
Molly <107332652+adjlfkld@users.noreply.github.com>
2022-11-08 09:56:35 +0900

    commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
-const text = document.querySelector(".text");
-// splitting text
-text.innerHTML = text.textContent.replace(
-  /\S/g,
-  "<span class='text2'>$&</span>"
-);
+const circle = document.querySelector("circle");
+const highfive = document.querySelector(".highfive");
 
-// rotating the span elements
-const element = document.querySelectorAll(".text2");
-for (let i = 0; i < element.length; i++) {
-  element[i].style.transform = "rotate(" + i * 17 + "deg)";
-  //   you can adjust the value based on the text length
+function movingHand(event) {
+  let mouseX = event.x;
+  let mouseY = event.y;
+  let handLeft = circle.getBoundingClientRect().left;
+  let handRight = circle.getBoundingClientRect().right;
+  let handTop = circle.getBoundingClientRect().top;
+  let handBottom = circle.getBoundingClientRect().bottom;
+  let handX = (handLeft + handRight) / 2;
+  let handY = (handTop + handBottom) / 2;
+  let x2 = mouseX - handX;
+  let y2 = mouseY - handY;
+
+  let Theta = Math.asin(x2 / Math.sqrt(x2 * x2 + y2 * y2));
+  if (y2 > 0) {
+    Theta = Math.PI - Theta;
+  }
+  circle.style.transform = `rotateZ(${Theta}rad)`;
+  console.log(Theta);
 }
+window.addEventListener("mousemove", movingHand);
+
+// x,y 가져오기 mouseX, mouseY
+// 손의 중앙 위치 가져오기 handX handY -> 완료
+// 위를 봤을때의 벡터 값 왜 정행 (0, -1) = v1
+// 마우스의 벡터 (mouseX-handX, mouseY-handY) =v2
+// mouseX-handX = x2 , mouseY-handY =y2
+// 각도 계산하기 Theta =Math.asin(x2 /1 * Math.sqrt(x2 * x2 + y2 * y2) )
+// transform : rotateZ 오른속법칙
