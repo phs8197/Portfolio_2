b26c964d735561f60085d784bcf1e22676fdfa8c
Molly <107332652+adjlfkld@users.noreply.github.com>
2022-11-11 17:17:53 +0900

    first commit

---
+++ js/jelly.js
@@ -0,0 +1,20 @@
+var section = document.querySelector("section");
+var boxes = document.querySelectorAll("#boxes > div");
+[].forEach.call(boxes, (box) => {
+  box.addEventListener("mousemove", (e) => {
+    section.style.setProperty(
+      "--bg-color",
+      box.style.getPropertyValue("--color")
+    );
+
+    var size = parseInt(getComputedStyle(box).width);
+
+    // scaling
+    var x = size * 0.3 * 0.7 + 0.7 * e.offsetX;
+    var y = size * 0.3 * 0.7 + 0.7 * e.offsetY;
+
+    box.style.setProperty("--x", x);
+    box.style.setProperty("--y", y);
+    box.style.setProperty("--size", size);
+  });
+});
