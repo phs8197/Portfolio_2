e6257928a3f9beb36023f661584b6a451a968f6b
Molly <107332652+adjlfkld@users.noreply.github.com>
2022-11-12 09:47:13 +0900

    commit

--- js/jelly.js
+++ js/jelly.js
@@ -12,7 +12,7 @@ var boxes = document.querySelectorAll("#boxes > div");
     // scaling
     var x = size * 0.3 * 0.7 + 0.7 * e.offsetX;
     var y = size * 0.3 * 0.7 + 0.7 * e.offsetY;
-
+    console.log(x);
     box.style.setProperty("--x", x);
     box.style.setProperty("--y", y);
     box.style.setProperty("--size", size);
